857737e23a861ef76332b913d10378ab03f95b4d
osmergit <osmer1976@gmail.com>
2022-11-15 21:57:12 -0500

    Componentes React Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CompCreateUser } from './deportes/Crearusuarios.jsx'
+import { CompShowUsers } from './deportes/mostrarusuarios.jsx'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <BrowserRouter>
+                      <Routes>
+                         
+                               <Route path='/create' element={ <CompCreateUser />} />
+                               <Route path='/users' element ={ <CompShowUsers />} />
+
+                      </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
